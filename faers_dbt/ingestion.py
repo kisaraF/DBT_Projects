8aef1b1306a4d0be0f76171d5385277ae527f5d6
Kisara Fernando <kisaraf@localhost.localdomain>
2025-11-08 12:58:53 +0530

    Audit columns added. Deduplication logic added. Used subprocess to move source zip files after processing

--- faers_dbt/ingestion.py
+++ faers_dbt/ingestion.py
@@ -2,15 +2,14 @@ import duckdb
 import polars as pl
 import logging
 import os
-
-# import subprocess
+import subprocess
 from datetime import datetime
 from zipfile import ZipFile
-# import io
 
 
 def init_logger() -> None:
-    log_fp = f"{os.getcwd()}/logs/log_ingestion.log"
+    log_date = datetime.now().strftime("%Y%m%d")
+    log_fp = f"{os.getcwd()}/logs/log_ingestion_{log_date}.log"
     logging.basicConfig(
         level=logging.DEBUG,
         filename=log_fp,
@@ -58,6 +57,30 @@ def init_db() -> None:
     return conn
 
 
+def get_hash_cols(tb_name: str) -> str:
+    if tb_name.upper() == "SRC_DEMO":
+        hash_cols = "concat(primaryid, init_fda_dt, fda_dt)"
+    elif tb_name.upper() == "SRC_DRUG":
+        hash_cols = "concat(primaryid, role_cod, drug_seq, drugname)"
+    elif tb_name.upper() == "SRC_REAC":
+        hash_cols = "concat(primaryid, pt)"
+    elif tb_name.upper() == "SRC_OUTC":
+        hash_cols = "concat(primaryid, outc_cod)"
+
+    return hash_cols
+
+
+def get_record_count(schema_name_i: str, tb_name: str) -> int:
+    conn = init_db()
+
+    get_count_q = f"""
+    SELECT COUNT(*)
+    FROM {schema_name_i}.{tb_name}
+    """
+    row_count = conn.execute(get_count_q).fetchone()[0]
+    return row_count
+
+
 def write_to_tables(data_dict: dict) -> None:
     con = init_db()
 
@@ -74,12 +97,14 @@ def write_to_tables(data_dict: dict) -> None:
     logging.info(f"{len(tables_ls)} tables are located in the database")
 
     for file in data_dict.keys():
-        logging.debug(f"processig {file}")
+        logging.debug(f"Processig {file}")
 
         df = pl.read_csv(
             data_dict[file], separator="$", has_header=True, infer_schema_length=0
         )
         logging.debug(f"{file} in polars data frame, has {len(df)} rows")
+
+        file_hash_cols_str = get_hash_cols(file)
 
         if file not in tables_ls:
             logging.debug(
@@ -88,19 +113,45 @@ def write_to_tables(data_dict: dict) -> None:
 
             query = f"""
             CREATE TABLE IF NOT EXISTS {schema_name}.{file} AS
-            SELECT * FROM df;
+            SELECT 
+                *,
+                SHA256(CONCAT({file_hash_cols_str})) AS _hash_id,
+                GET_CURRENT_TIMESTAMP() as _created_at
+            FROM df;
             """
             con.execute(query)
             logging.info(f"Created {file} and Inserted {len(df)} records")
         elif file in tables_ls:
-            logging.debug(f"The table {file} is available. Inserting new data..")
+            rec_count_prev = get_record_count(schema_name, file)
+            logging.debug(
+                f"The table {file} is available and has {rec_count_prev} records. Inserting new data.."
+            )
 
             query = f"""
             INSERT INTO {schema_name}.{file}
-            SELECT * FROM df;
+            SELECT 
+                *,
+                SHA256(CONCAT({file_hash_cols_str})) AS _hash_id,
+                GET_CURRENT_TIMESTAMP() as _created_at 
+            FROM df
+            WHERE _hash_id not in (
+                SELECT DISTINCT _hash_id
+                FROM {schema_name}.{file}
+            );
             """
             con.execute(query)
-            logging.info(f"Inserted {len(df)} records to table {file}")
+
+            rec_count_aft = get_record_count(schema_name, file)
+            logging.info(
+                f"Inserted {rec_count_aft - rec_count_prev} records to table {file}. Deduplicated {(rec_count_aft - rec_count_prev) - len(df)} records"
+            )
+
+
+def move_after_writing(file_name: str) -> None:
+    move_destination = f"{os.getcwd()}/Data/Archive/"
+    cmd_keywords = ["mv", file_name, move_destination]
+    subprocess.run(cmd_keywords)
+    logging.info(f"{file_name} moved to {move_destination} after writing")
 
 
 if __name__ == "__main__":
@@ -114,3 +165,4 @@ if __name__ == "__main__":
         logging.info(f"Processing batch -> {batch}")
         data_dict = process_file(batch)
         write_to_tables(data_dict)
+        move_after_writing(batch)
